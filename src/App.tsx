80bc9edb51700633f638d29cf7d6210e12f33c06
Ibrahim Irsad <ibrairsyad1712@gmail.com>
2024-08-13 07:22:04 +0700

    fix: props type error

--- src/App.tsx
+++ src/App.tsx
@@ -56,7 +56,7 @@ function App() {
             path="/dashboard"
             element={
               isAuthenticated ? (
-                <Dashboard onLogout={handleLogout} mode={mode} />
+                <Dashboard onLogout={handleLogout} />
               ) : (
                 <Login
                   onLogin={(username, password) => {
